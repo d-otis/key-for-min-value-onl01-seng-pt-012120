ef043aece3dfc21c206b25624f50e8302864a598
Dan Foley <dan@dan-foley.com>
2020-01-28 16:44:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,9 @@
 def key_for_min_value(name_hash)
   # if name_hash empty return nil
   # else if value is lower than stored value return the key for that value
+  if name_hash.empty?
+    return nil
+  else
+    
+  end
 end
