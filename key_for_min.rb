51b082546c00cc844711596c42b3ea3abef9d86b
Dan Foley <dan@dan-foley.com>
2020-01-28 17:06:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,13 +12,14 @@ def key_for_min_value(name_hash)
     min_value = nil
     min_key = ""
     name_hash.collect do |key, value|
-      binding.pry
+      # binding.pry
       if !min_value
         min_value = value
         min_key = key
       elsif value < min_value
         min_value = value
         min_key = key
+        binding.pry
       end
     end
     min_key
