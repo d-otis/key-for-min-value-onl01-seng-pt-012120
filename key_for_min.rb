64af2acac2d68d9e40fd37238067b04ba9f3e86f
Dan Foley <dan@dan-foley.com>
2020-01-28 16:41:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       # puts "key is #{key} and value is #{value}"
       if value < min_value
-        min_value = value
+        min_value_key = key
       end
     end
   end
